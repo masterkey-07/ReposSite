49bc6f219d6067f8290a5484a21cfea96fedfc95
VICTOR JORGE CARVALHO CHAVES <victor.chaves@eleb.net>
2021-04-18 14:32:03 -0300

    File organized

--- web/server.js
+++ web/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/", require("./server/api/"));
+app.use("/api/", require("./server/api"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening on Port: ${PORT}`));
